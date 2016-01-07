fa520dcd9cd6a10584b19c2502a7c017f4e292a1
freedomkiller <apple@AppledeMac-mini.local>
2016-01-07 15:05:44 +0800

    QRCode
    ZBar-QRCode

--- 二维码/ViewController.m
+++ 二维码/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ZCZBarViewController.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIButton * button = [[UIButton alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    [button setTitle:@"扫描二维码" forState:UIControlStateNormal];
+    [self.view addSubview:button];
+    [button addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    button.backgroundColor = [UIColor greenColor];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)buttonClick
+{
+    ZCZBarViewController * vc = [[ZCZBarViewController alloc]initWithBlock:^(NSString * str, BOOL isSucceed) {
+        if (isSucceed) {
+            NSLog(@"扫描完成");
+        }
+    }];
+    [self presentViewController:vc animated:YES completion:nil];
+    
+    vc = nil;
+
 }
 
 - (void)didReceiveMemoryWarning {
